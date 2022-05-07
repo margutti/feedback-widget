d1035369452a41c7b4299e6574c7d054f01bbaed
Sergio Roberto Margutti <margutti@softmidia.com.br>
2022-05-07 17:48:31 -0300

    Sent e-mail (nodemailer)

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,19 @@
 import { prisma } from './prisma';
 import express from 'express';
+import nodemailer from 'nodemailer';
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "d9dff017e411f7",
+      pass: "c414856277c47d"
+    }
+  });
 
 app.post('/feedbacks', async (req, res) => {
     const { type, comment, screenshot } = req.body;
@@ -14,6 +24,18 @@ app.post('/feedbacks', async (req, res) => {
             comment,
             screenshot,
         }
+    });
+
+    await transport.sendMail({
+        from: 'Equipe Feedget <oi@feedget.com>',
+        to: 'Sergio Margutti <margutti@softmidia.com.br>',
+        subject: 'Novo feedback',
+        html: [
+            `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+            `<p>Tipo do feedback: ${type}</p>`,
+            `<p>Comentário: ${comment}</>`,
+            `</div>`
+        ].join('\n')
     });
 
     return res.status(201).json({ data: feedback });
